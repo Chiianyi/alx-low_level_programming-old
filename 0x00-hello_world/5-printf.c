f4510053009f8a818ec5ea7d0d9b7214c30b4d49
Chiianyi <chibuikemjaynkemakonam@gmail.com>
2023-08-11 15:49:41 -0700

    lots of warnings on the siize project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - entry point
- *
- * return: 0 for success
+ *my output, printf
+ * return: Always 0 (success)
  */
 int main(void)
 {
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *render variable sizes, printf
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("size of char : %c\n", sizeof(char));
+	printf("size of int : %d\n", sizeof(int));
+	printf("size of long int : %ld\n", sizeof(long int));
+	printf("size of long long int : %lld\n", sizeof(long long int));
+	printf("size of float : %f\n", sizeof(float));
+	return (0);
+
+}
+
+
