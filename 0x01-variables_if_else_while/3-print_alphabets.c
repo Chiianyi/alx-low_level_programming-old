a6d70c78879b9e58daaa76d51a7cf63b205d0104
Chiianyi <chibuikemjaynkemakonam@gmail.com>
2023-08-14 10:12:08 -0700

    two while loops makes  an alphabet soup

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: Use ASCII to print lowercase alphbts
+ * Description: Use ASCII to print up u and l case alphbts
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n = 97;
+	int m = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
 	}
 	putchar('\n');
 	return (0);
