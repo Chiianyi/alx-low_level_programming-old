00303aac1944ed84fc44b79425ad340345d8582c
Chiianyi <chibuikemjaynkemakonam@gmail.com>
2023-08-14 09:40:22 -0700

    Use putchar to   push our alphabets

---
+++ 0x01-variables_if_else_while/2-touch_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print lowercase alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
