dc1b6ed0b730648b25b7397510edd6a73dc707ac
Chiianyi <chibuikemjaynkemakonam@gmail.com>
2023-08-24 20:18:19 -0700

    Strmcpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,45 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - Check the code
+ * _strncpy - COpies at most an inputed number
+ * of bytes from stirng into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to be copied from src.
+ * Return: A pointer to the resulting string dest.
  *
- * Return: Always 0.
  */
 
-int main(void)
+char *_strncpy(char *dest, char *src, int n)
+
 {
-	char s1[98];
-	char *ptr;
-	int i;
+	int index  = 0, src_len = 0;
 
-	for (i = 0; i < 98 - 1; i++)
-	{
-		s1[i] = '*';
-	}
-	s1[i] = '\0';
-	printf("%s\n", s1);
-	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-	printf("%s\n", s1);
-	printf("%s\n", ptr);
-	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 98);
-	printf("%s", s1);
-	printf("%s", ptr);
+	while (src[index++])
+		src_len++;
 
-	for (i = 0; i < 98; i++)
+	for (index = 0; src[index] && index < n; index++)
+	dest[index] = src[index];
 
-	{
-		if (i % 10)
-		{
-			printf(" ");
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", s1[i]);
-	}
-	printf("\n");
-	return (0);
+	return (dest);
 }
