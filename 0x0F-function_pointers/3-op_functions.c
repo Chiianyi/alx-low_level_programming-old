b3d2f131fba8fe8d7a8e122c1a4a094e94f66a81
Chiianyi <chibuikemjaynkemakonam@gmail.com>
2023-09-13 06:07:58 -0700

    0x0F 4

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition function
+ * @a: first int input
+ * @b: second int input
+ * Return: sum of x and y
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - subtraction function
+ * @a: first int
+ * @b: second int
+ * Return: the difference of two integers
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - calculates the products of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: product of two numbers
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first num
+ * @b: second num
+ * Return: returns division of two nums
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
